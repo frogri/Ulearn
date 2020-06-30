1f594987b39e04e06c9e9a472160c6c4f817c435
FGV <frogri_apps@mail.ru>
2020-06-30 08:43:52 +0400

    Перезалиты проекты "04_Upravlenie_robotom" и "Practice_01_Pluralize"

---
+++ 03_Vetvlenija/04_Upravlenie_robotom/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+ В воскресенье Вася пошел в кружок робототехники и увидел там такой код управления боевым роботом:
+
+
+
+ static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+{
+	bool shouldFire = true;
+	if (enemyInFront == true)
+	{
+		if (enemyName == "boss")
+		{
+			if (robotHealth < 50) shouldFire = false;
+			if (robotHealth > 100) shouldFire = true;
+		}
+	}
+	else
+	{
+		return false;
+	}
+	return shouldFire;
+}
+ 
+
+Код показался Васе слишком длинным, а к тому же еще и неряшливым. Вася поспорил с автором, что сможет написать функцию, делающую ровно то же самое, но всего в один оператор.
+Кажется, Вася погорячился... Или нет? Помогите ему не проиграть в споре!
+
+
+
+Продолжите предложение: 'стрелять нужно в случае, если...'
+Грамотно используйте конъюнкцию вместе с дизъюнкцией
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upravlenie_robotom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            bool shouldFire = true;
+            if (enemyInFront == true)
+            {
+                if (enemyName == "boss")
+                {
+                    if (robotHealth < 50) shouldFire = false;
+                    if (robotHealth > 100) shouldFire = true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return shouldFire;
+        }
+
+
+
+        static bool ShouldFire2(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            return enemyInFront && ((enemyName == "boss" && robotHealth >= 50) || enemyName != "boss");
+        }
+    }
+}
---
+++ 03_Vetvlenija/Practice_01_Pluralize/PluralizeTask.cs
@@ -0,0 +1,41 @@
+﻿//Напишите метод, склоняющий существительное «рублей» следующее за указанным числительным.
+//Например, для аргумента 10, метод должен вернуть «рублей», для 1 — вернуть «рубль», для 2 — «рубля».
+//Для проверки своего решения запустите скачанный проект.
+
+using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Pluralize
+{
+    public static class PluralizeTask
+    {
+        public static string PluralizeRubles(int count)
+        {
+            // Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+            string countToString = count.ToString();
+            //берем последний символ строки
+            char lastChar = countToString.Last();
+
+            //если строка состоит из одного символа
+            if (countToString.Length == 1)
+                return LastCharSearching(lastChar);
+
+            //берем предпоследний символ
+            char nextToLastChar = countToString.ElementAt(countToString.Length - 2);
+            if (nextToLastChar != '1')
+                return LastCharSearching(lastChar);
+            return "рублей";
+        }
+
+        public static string LastCharSearching(char lastChar)
+        {
+            if (lastChar == '1')
+                return "рубль";
+            else if (lastChar == '2' || lastChar == '3' || lastChar == '4')
+                return "рубля";
+            else
+                return "рублей";
+        }
+    }
+}
