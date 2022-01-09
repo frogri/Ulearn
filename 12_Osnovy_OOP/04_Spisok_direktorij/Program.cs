5489521ff003e10bdd7fc96019ce2b75586406da
g.v.f <frogri_apps@mail.ru>
2022-01-09 18:03:29 +0400

    Решена задача "12_Osnovy_OOP/04_Spisok_direktorij"

---
+++ 12_Osnovy_OOP/04_Spisok_direktorij/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Так получилось, что вы пишете духовную операционную систему "Оконце, версия 0.99", 
+и модуль к этой системе, отвечающий за составление медиа-альбомов пользователя.
+
+По данному вам списку файлов составьте список всех директорий, 
+которые содержат файлы с расширением .mp3 и .wav.
+
+Каждую директорию нужно вернуть только один раз, порядок значения не имеет.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _04_Spisok_direktorij
+{
+    internal class Program
+    {
+        public static List<DirectoryInfo> GetAlbums(List<FileInfo> files)
+        {
+            var albums = new List<DirectoryInfo>();
+
+            foreach (var file in files)
+            {
+                if (!file.Extension.Equals(".mp3") && !file.Extension.Equals(".wav"))
+                    continue;
+
+                if (!albums.Exists(item => item.FullName == file.DirectoryName))
+                    albums.Add(file.Directory);
+            }
+
+            return albums;
+        }
+
+        private static void Main(string[] args)
+        {
+            var files = new List<FileInfo>();
+            var path = Path.Combine(Environment.CurrentDirectory, "..\\..\\Files");
+
+            var dirInfo = new DirectoryInfo(path);
+            foreach (var directory in dirInfo.GetDirectories()) files.AddRange(directory.GetFiles());
+
+            GetAlbums(files);
+
+            Console.ReadKey();
+        }
+
+        //public static List<DirectoryInfo> GetAlbums(List<FileInfo> files)
+        //{
+        //    var albums = new List<DirectoryInfo>();
+        //    var mediaFiles = files.Where(info => info.Extension.Equals(".mp3") || info.Extension.Equals(".wav"));
+
+        //    foreach (var file in mediaFiles)
+        //    {
+        //        if (!albums.Exists(item => item.FullName == file.DirectoryName))
+        //            albums.Add(file.Directory);
+        //    }
+
+        //    return albums;
+        //}
+    }
+}
