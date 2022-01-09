33dae204d0fc9b2eca60188a8bdf0a5db3278c69
g.v.f <frogri_apps@mail.ru>
2022-01-09 15:32:14 +0400

    Решена задача "12_Osnovy_OOP/03_Sozdanie_metodov_rasshireniya"

---
+++ 12_Osnovy_OOP/03_Sozdanie_metodov_rasshireniya/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * И снова сделайте так, чтобы код заработал!
+ *
+ * Создайте метод расширения класса String, преобразующий строку в int.
+ * Метод расширения должен быть определен в статическом классе, а перед его первым аргументом должно быть ключевое слово this
+ */
+
+using System;
+
+namespace _03_Sozdanie_metodov_rasshireniya
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var arg1 = "100500";
+            Console.Write(arg1.ToInt() + "42".ToInt()); // 100542
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static int ToInt(this string str)
+        {
+            return Convert.ToInt32(str);
+        }
+    }
+}
