a49ec7914f96207fef040930f4f1a8580902e74a
g.v.f <frogri_apps@mail.ru>
2022-01-09 18:35:39 +0400

    Решена задача "12_Osnovy_OOP/05_Refaktoring_staticheskogo_klassa"

---
+++ 12_Osnovy_OOP/05_Refaktoring_staticheskogo_klassa/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Сделайте так, чтобы код заработал!
+ * Для этого сделайте класс SuperBeautyImageFilter не статическим.
+ */
+
+using System;
+using System.Globalization;
+
+namespace _05_Refaktoring_staticheskogo_klassa
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var filter = new SuperBeautyImageFilter();
+            filter.ImageName = "Paris.jpg";
+            filter.GaussianParameter = 0.4;
+            filter.Run();
+
+            Console.ReadKey();
+        }
+
+        public class SuperBeautyImageFilter
+        {
+            public string ImageName;
+            public double GaussianParameter;
+
+            public void Run()
+            {
+                Console.WriteLine("Processing {0} with parameter {1}",
+                                  ImageName,
+                                  GaussianParameter.ToString(CultureInfo.InvariantCulture));
+                //do something useful
+            }
+        }
+    }
+}
