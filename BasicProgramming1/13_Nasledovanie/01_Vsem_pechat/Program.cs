ff4cacb8953dbd9bb66bd161bef4b756f79a1e79
g.v.frolov <g.v.frolov@kalashnikovconcern.ru>
2022-02-07 10:27:49 +0400

    Решены задачы "13_Nasledovanie/01_Vsem_pechat" и "13_Nasledovanie/02_Sklejka_massivov"

---
+++ BasicProgramming1/13_Nasledovanie/01_Vsem_pechat/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите метод, который печатает все, что угодно, через запятую.
+
+using System;
+
+namespace _01_Vsem_pechat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Print(1, 2);
+            Print("a", 'b');
+            Print(1, "a");
+            Print(true, "a", 1);
+
+            Console.ReadKey();
+        }
+
+        public static void Print(params object[] elements)
+        {
+            for (var i = 0; i < elements.Length; i++)
+            {
+                if (i > 0)
+                    Console.Write(", ");
+                Console.Write(elements[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
