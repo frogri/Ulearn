f0edb3713e2fc506b48a04135a46b7bcf07d2094
g.v.frolov <g.v.frolov@kalashnikovconcern.ru>
2022-02-08 08:53:27 +0400

    Решена задача "13_Nasledovanie/04_Poisk_minimuma"

---
+++ BasicProgramming1/13_Nasledovanie/04_Poisk_minimuma/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите метод Min, который бы вычислял минимум из элементов массива.
+
+using System;
+
+namespace _04_Poisk_minimuma
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Min(new[] { 3, 6, 2, 4 }));
+            Console.WriteLine(Min(new[] { "B", "A", "C", "D" }));
+            Console.WriteLine(Min(new[] { '4', '2', '7' }));
+
+            Console.ReadKey();
+        }
+
+        private static object Min(Array args)
+        {
+            var min = (IComparable)args.GetValue(0);
+            
+            foreach (var argument in args)
+            {
+                var comparableArgument = (IComparable)argument;
+                
+                if (comparableArgument.CompareTo(min) < 0)
+                    min = comparableArgument;
+            }
+
+            return min;
+        }
+    }
+}
