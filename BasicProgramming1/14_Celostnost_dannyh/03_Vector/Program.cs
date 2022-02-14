0d7e3b1ff0a54592aa86da06368ad6193cbf23b9
g.v.frolov <g.v.frolov@kalashnikovconcern.ru>
2022-02-14 11:26:06 +0400

    Решена задача "14_Celostnost_dannyh/03_Vector"

---
+++ BasicProgramming1/14_Celostnost_dannyh/03_Vector/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Добавьте конструктор в класс Vector.
+Сделайте так, чтобы:
+
+поля этого класса инициализировались в конструкторе.
+поле Length (длина вектора), стало вычисляемым свойством.
+*/
+
+using System;
+
+namespace _03_Vector
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Check();
+            Console.ReadKey();
+        }
+
+        public static void Check()
+        {
+            Vector vector = new Vector(3, 4);
+            Console.WriteLine(vector.ToString());
+
+            vector.X = 0;
+            vector.Y = -1;
+            Console.WriteLine(vector.ToString());
+
+            vector = new Vector(9, 40);
+            Console.WriteLine(vector.ToString());
+
+            Console.WriteLine(new Vector(0, 0).ToString());
+        }
+
+        public class Vector
+        {
+            public double X;
+            public double Y;
+
+            public double Length => Math.Sqrt(X * X + Y * Y);
+
+            public Vector(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("({0}, {1}) with length: {2}", X, Y, Length);
+            }
+        }
+    }
+}
