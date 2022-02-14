3ad382f873a2dabb762cc68445c4ae264c9b7f82
g.v.frolov <g.v.frolov@kalashnikovconcern.ru>
2022-02-14 10:21:46 +0400

    Решена задача "14_Celostnost_dannyh/02_Svojstvo_vmesto_polya"

---
+++ BasicProgramming1/14_Celostnost_dannyh/02_Svojstvo_vmesto_polya/Program.cs
@@ -0,0 +1,27 @@
+﻿// Превратите поле Title в свойство самостоятельно. Это совсем не сложно!
+
+using System;
+
+namespace _02_Svojstvo_vmesto_polya
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Check();
+            Console.ReadKey();
+        }
+
+        public static void Check()
+        {
+            var book = new Book();
+            book.Title = "Structure and interpretation of computer programs";
+            Console.WriteLine(book.Title);
+        }
+
+        public class Book
+        {
+            public string Title { get; set; }
+        }
+    }
+}
