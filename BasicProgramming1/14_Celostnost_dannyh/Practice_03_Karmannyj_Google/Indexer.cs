e966a379ec880a646e988b240861d2e78f29afb0
g.v.frolov <g.v.frolov@kalashnikovconcern.ru>
2022-02-15 16:28:16 +0400

    Добавлена задача "14_Celostnost_dannyh/Practice_03_Karmannyj_Google"

---
+++ BasicProgramming1/14_Celostnost_dannyh/Practice_03_Karmannyj_Google/Indexer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PocketGoogle
+{
+    public class Indexer : IIndexer
+    {
+        public void Add(int id, string documentText)
+        {
+            var separators = new[] { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' };
+            var array = documentText.Split(separators);
+
+            //var result = array.Where(s => !string.IsNullOrEmpty(s)).ToDictionary(id, item);
+
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetIds(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetPositions(int id, string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
