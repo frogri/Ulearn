d5ea48a4af88a05895e2f4a1b6c16ae897a9e88a
g.v.frolov <g.v.frolov@kalashnikovconcern.ru>
2022-03-17 13:00:44 +0400

    Решена задача "02_Yield_return/01_Generaciya_posledovatelnosti"

---
+++ BasicProgramming2/02_Yield_return/01_Generaciya_posledovatelnosti/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Ленивые методы полезны тем, что они могу генерировать последовательности до тех пор пока нам это необходимо. 
+В данном упражнении реализуйте функцию, которая лениво генерирует циклическую последовательность через yield return.
+
+Пример: При поочередном вызове метода GenerateCycle(3) должна возвращаться последовательность { 0, 1, 2, 0, 1, 2, 0, 1...}
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Generaciya_posledovatelnosti
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var number in GenerateCycle(4).Take(8))
+                Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<int> GenerateCycle(int maxValue)
+        {
+            var current = 0;
+
+            while (true)
+            {
+                yield return current;
+
+                ++current;
+                current = current < maxValue ? current : 0;
+            }
+        }
+    }
+}
