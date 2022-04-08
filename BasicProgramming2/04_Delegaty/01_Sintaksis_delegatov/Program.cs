639450a80c11fb2f1dc727e39d4d47a85c39dfe4
g.v.f <frogri_apps@mail.ru>
2022-04-08 06:00:32 +0400

    Решена задача "04_Delegaty/01_Sintaksis_delegatov"

--- BasicProgramming2/04_Delegaty/01_Sintaksis_delegatov/Program.cs
+++ BasicProgramming2/04_Delegaty/01_Sintaksis_delegatov/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+Наша программа умеет общаться с пользователем, 
+но конкретный способ общения задается параметром tellUser. 
+Так с помощью делегатов можно обобщать код.
+
+Осталось только дописать определение типа TellUser так, 
+чтобы код скомпилировался. 
+Эта простая задача поможет вам запомнить синтаксис определения делегата.
+ */
+
+using System;
 
 namespace _01_Sintaksis_delegatov
 {
-    class Program
+    public delegate void TellUser(string str);
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Run(Console.WriteLine);
+
+            Console.ReadKey();
+        }
+
+        static void Run(TellUser tellUser)
+        {
+            tellUser("hi!");
+            tellUser("how r u?");
         }
     }
 }
