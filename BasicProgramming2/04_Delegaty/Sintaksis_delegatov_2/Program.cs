d7cc9b60950405c53bc31b013825033c341fc014
g.v.f <frogri_apps@mail.ru>
2022-04-18 04:59:02 +0400

    Решена задача "04_Delegaty/02_Sintaksis_delegatov_2"

---
+++ BasicProgramming2/04_Delegaty/Sintaksis_delegatov_2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Можно было не создавать свой делегат TellUser в прошлой задаче, а использовать Action. 
+Но в некоторых ситуациях Action и Func недостаточно.
+
+Задача все та же — напишите определение делегата TryGet так, чтобы код скомпилировался!
+ */
+
+using System;
+
+namespace Sintaksis_delegatov_2
+{
+    internal class Program
+    {
+        public delegate bool TryGet<T1, T2>(T1 argument, Action<string> action, out T2 result);
+
+        static void Main()
+        {
+            Run(AskUser, Console.WriteLine);
+
+            Console.ReadKey();
+        }
+
+        static void Run(TryGet<string, int> askUser, Action<string> tellUser)
+        {
+            int age;
+            if (askUser("What is your age?", tellUser, out age))
+                tellUser("Age: " + age);
+        }
+
+        static bool AskUser(string questionText, Action<string> tellUser, out int age)
+        {
+            tellUser(questionText);
+            var answer = Console.ReadLine();
+            return int.TryParse(answer, out age);
+        }
+	}
+}
