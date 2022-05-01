0232a103ccd8d1624a6d3dee25442e7aa53b9d48
g.v.f <frogri_apps@mail.ru>
2022-05-01 16:01:45 +0400

    Решена задача "05_Elementy_funkcionalnogo_programmirovaniya/02_Realizaciya_metoda_Take"

---
+++ BasicProgramming2/05_Elementy_funkcionalnogo_programmirovaniya/02_Realizaciya_metoda_Take/Program.cs
@@ -0,0 +1,51 @@
+﻿// Реализуйте метод Take с использованием yield return,
+// который принимает на вход последовательность source и число count,
+// а возвращает последовательность только из первых count элементов source.
+
+// Если source содержит меньше count элементов, то вернуть нужно все элементы source.
+// Подсказки
+// 1. Take считается недостаточно ленивым даже если он извлекает из source всего один лишний элемент.
+// 2. foreach в начале каждой итерации извлекает из source очередной элемент, чтобы сложить его в переменную цикла.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace _02_Realizaciya_metoda_Take
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Func<int[], int, string> take = (source, count) => string.Join(" ", Take(source, count));
+
+            Assert.AreEqual("1 2", take(new[] { 1, 2, 3, 4 }, 2));
+            Assert.AreEqual("4", take(new[] { 4 }, 1));
+            Assert.AreEqual("", take(new[] { 5 }, 0));
+
+            var num = new Random().Next(0, 1000);
+            Assert.AreEqual(num.ToString(), take(new[] { num }, 100500));
+
+            //CheckLazyness();
+            Console.WriteLine("OK");
+        }
+
+        private static IEnumerable<T> Take<T>(IEnumerable<T> source, int count)
+        {
+            if (count == 0)
+                yield break;
+
+            var i = 0;
+
+            foreach (var item in source)
+            {
+                i++;
+                yield return item;
+
+                if (i >= count)
+                    yield break;
+            }
+        }
+    }
+}
