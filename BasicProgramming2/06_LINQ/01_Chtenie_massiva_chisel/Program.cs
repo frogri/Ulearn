249f16b059f3e452309bdf3504ffc35c3952cdf5
g.v.f <frogri_apps@mail.ru>
2022-05-04 20:49:18 +0400

    Решена задача "06_LINQ/01_Chtenie_massiva_chisel"

---
+++ BasicProgramming2/06_LINQ/01_Chtenie_massiva_chisel/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+LINQ удобно использовать для чтения из файла и разбора простых текстовых форматов. 
+Особенно удобно сочетать методы LINQ с методами класса File: File.ReadLines(filename), File.WriteLines(filename, lines).
+
+На выходе метода ReadAllLines получается массив строк, 
+поэтому часто возникает задача обработки именно массива строк.
+
+Пусть у вас есть файл, в котором каждая строка либо пустая, 
+либо содержит одно целое число. Напишите метод, который вернет массив всех чисел, присутствующих в исходном списке строк.
+
+Ваш код будет проверяться с помощью метода Main:
+
+Реализуйте метод ParseNumbers в одно LINQ-выражение.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Chtenie_massiva_chisel
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            foreach (var num in ParseNumbers(new[] { "-0", "+0000" }))
+                Console.WriteLine(num);
+
+            foreach (var num in ParseNumbers(new List<string> { "1", "", "-03", "0" }))
+                Console.WriteLine(num);
+        }
+
+        public static int[] ParseNumbers(IEnumerable<string> lines)
+        {
+            return lines.Where(s => s != string.Empty)
+                .Select(s => int.Parse(s))
+                .ToArray();
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace Test
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
 {
     internal class Program
     {
+        static IEnumerable<int> GetSequence()
+        {
+            for (var i = 0; i < 2; ++i)
+            {
+                Console.WriteLine("s");
+                yield return i;
+            }
+        }
+
         static void Main(string[] args)
         {
+            foreach (var element in GetSequence().ToList().Take(1))
+                Console.WriteLine("f");
+
+            Console.ReadKey();
         }
     }
 }
