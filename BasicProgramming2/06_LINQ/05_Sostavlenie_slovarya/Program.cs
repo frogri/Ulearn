0cd1ac37143ba4520862f82d073293f98f632cdf
g.v.frolov <g.v.frolov@kalashnikovconcern.ru>
2022-05-16 09:35:03 +0400

    Решена задача "06_LINQ/05_Sostavlenie_slovarya"

---
+++ BasicProgramming2/06_LINQ/05_Sostavlenie_slovarya/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Текст задан массивом строк. 
+Вам нужно составить лексикографически упорядоченный список всех встречающихся в этом тексте слов.
+
+Слова нужно сравнивать регистронезависимо, а выводить в нижнем регистре.
+*/
+
+using System;
+using System.Linq;
+
+namespace _05_Sostavlenie_slovarya
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var vocabulary = GetSortedWords("Hello, hello, hello, how low",
+                                            string.Empty,
+                                            "With the lights out, it's less dangerous",
+                                            "Here we are now; entertain us",
+                                            "I feel stupid and contagious",
+                                            "Here we are now; entertain us",
+                                            "A mulatto, an albino, a mosquito, my libido...",
+                                            "Yeah, hey");
+            foreach (var word in vocabulary)
+                Console.WriteLine(word);
+
+            Console.ReadKey();
+        }
+
+        public static string[] GetSortedWords(params string[] textLines)
+        {
+            return textLines
+                .SelectMany(line => line.Split(',', ';', ' ', '.', '\''))
+                .Where(word => !string.IsNullOrWhiteSpace(word))
+                .Select(word => word.ToLower())
+                .Distinct()
+                .OrderBy(word => word)
+                .ToArray();
+        }
+    }
+}
